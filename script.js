8ac4c0b1e72c09b1e674a9f79b3d83b5abc2b558
VincentChen6345 <vincent.chen6345@hotmail.com>
2025-03-30 12:19:01 +1100

    cleanup

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-console.log("hello world");
-
-const btn = document.querySelector(".btn-open");
-const form = document.querySelector(".fact-form");
-btn.addEventListener("click", function () {
-  if (form.classList.contains("hidden")) {
-    form.classList.remove("hidden");
-    btn.textContent = "Close";
-  } else {
-    form.classList.add("hidden");
-    btn.textContent = "Share a fact";
-  }
-});
-
-let votesInteresting = 23;
-let votesMindblowing = 5;
-const text = "Listbon is the capital of Portugal";
-
-votesInteresting = votesInteresting + 1;
-
-let totalUpvotes = votesInteresting + votesMindblowing;
-console.log("Upvotes:", totalUpvotes);
-let votesFalse = 4;
-const isCorrect = votesFalse < totalUpvotes;
-console.log(isCorrect);
-=======
 const initialFacts = [
   {
     id: 1,
@@ -152,4 +125,3 @@ function createFactsList(dataArray) {
     }
   });
 }
->>>>>>> recovered-branch
